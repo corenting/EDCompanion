5c7a389013a40ed1c668bf6a8a19446c2ccab295
Corentin Garcia <corentin.garcia@3ie.fr>
2017-03-31 13:05:04 +0200

    Tweaked theme

--- app/src/main/java/fr/corenting/edcompanion/activities/MainActivity.java
+++ app/src/main/java/fr/corenting/edcompanion/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package fr.corenting.edcompanion.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -13,12 +15,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import fr.corenting.edcompanion.R;
 import fr.corenting.edcompanion.fragments.CommunityGoalsFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private  FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +42,18 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        // Fragment
-        CommunityGoalsFragment fragment = new CommunityGoalsFragment();
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragmentContent, fragment).commit();
-
+        // Setup navigation view and fake click the first item
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Set initial fragment
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager
+                .beginTransaction().replace(R.id.fragmentContent, new CommunityGoalsFragment())
+                .commit();
+
+        // Select the first item in menu as the fragment was loaded
+        navigationView.setCheckedItem(navigationView.getMenu().getItem(0).getItemId());
     }
 
     @Override
@@ -81,21 +93,15 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_cg) {
+            fragmentManager
+                    .beginTransaction().replace(R.id.fragmentContent, new CommunityGoalsFragment())
+                    .commit();
+        } else if (id == R.id.nav_galnet) {
+            fragmentManager
+                    .beginTransaction().replace(R.id.fragmentContent, new CommunityGoalsFragment())
+                    .commit();
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
