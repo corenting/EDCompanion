90f00fc5ddbb1c4fdfca313777ac2302b66640dd
Corentin Garcia <corentin.garcia@3ie.fr>
2017-05-24 18:57:24 +0200

    Added layout for rank item in status fragment

---
+++ app/src/main/java/fr/corenting/edcompanion/fragments/StatusFragment.java
@@ -0,0 +1,97 @@
+package fr.corenting.edcompanion.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import fr.corenting.edcompanion.R;
+import fr.corenting.edcompanion.adapters.GalnetAdapter;
+import fr.corenting.edcompanion.models.GalnetNews;
+import fr.corenting.edcompanion.network.GalnetNetwork;
+
+public class StatusFragment extends Fragment  {
+
+    @BindView(R.id.recyclerView)
+    public RecyclerView recyclerView;
+    @BindView(R.id.swipeContainer)
+    public SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.emptySwipe)
+    public SwipeRefreshLayout emptySwipeRefreshLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_community_goals, container, false);
+        ButterKnife.bind(this, v);
+
+        // Recycler view setup
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(new GalnetAdapter(getContext()));
+
+        //Swipe to refresh setup
+        final StatusFragment parent = this;
+        SwipeRefreshLayout.OnRefreshListener listener = new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                GalnetAdapter adapter = (GalnetAdapter) recyclerView.getAdapter();
+                endLoading(0);
+                adapter.clearNews();
+                emptySwipeRefreshLayout.setVisibility(View.GONE);
+                swipeRefreshLayout.setVisibility(View.VISIBLE);
+                swipeRefreshLayout.setRefreshing(true);
+                GalnetNetwork.getNews(parent);
+            }
+        };
+        swipeRefreshLayout.setOnRefreshListener(listener);
+        emptySwipeRefreshLayout.setOnRefreshListener(listener);
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Setup views
+        emptySwipeRefreshLayout.setVisibility(View.GONE);
+        swipeRefreshLayout.setVisibility(View.VISIBLE);
+        swipeRefreshLayout.setRefreshing(true);
+
+        // Register event and get the news
+        EventBus.getDefault().register(this);
+        GalnetNetwork.getNews(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe
+    public void onNewsEvent(GalnetNews news) {
+        GalnetAdapter adapter = (GalnetAdapter) recyclerView.getAdapter();
+        adapter.addNews(news);
+    }
+
+    public void endLoading(int count)
+    {
+        swipeRefreshLayout.setRefreshing(false);
+        emptySwipeRefreshLayout.setRefreshing(false);
+        if (count <= 0)
+        {
+            emptySwipeRefreshLayout.setVisibility(View.VISIBLE);
+            swipeRefreshLayout.setVisibility(View.GONE);
+        }
+    }
+}
