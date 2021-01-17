3ec4b926ff2dbae8acfa19be7a77ce821b4082a2
corenting <corenting@gmail.com>
2021-01-17 20:10:05 +0100

    Share system input text between views

--- app/src/main/java/fr/corenting/edcompanion/views/SystemInputView.kt
+++ app/src/main/java/fr/corenting/edcompanion/views/SystemInputView.kt
@@ -3,13 +3,17 @@ package fr.corenting.edcompanion.views
 import android.app.Activity
 import android.content.Context
 import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
+import androidx.core.widget.addTextChangedListener
+import fr.corenting.edcompanion.R
 import fr.corenting.edcompanion.adapters.AutoCompleteAdapter
 import fr.corenting.edcompanion.models.events.CommanderPosition
 import fr.corenting.edcompanion.utils.NotificationsUtils
 import fr.corenting.edcompanion.utils.PlayerNetworkUtils
+import fr.corenting.edcompanion.utils.SettingsUtils
 import kotlinx.android.synthetic.main.view_system_input.view.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -38,16 +42,29 @@ class SystemInputView : RelativeLayout {
 
 
         if (attrs != null) {
-            val a = context.obtainStyledAttributes(attrs,
-                    fr.corenting.edcompanion.R.styleable.SystemInputView, 0, 0)
+            val a = context.obtainStyledAttributes(
+                attrs,
+                R.styleable.SystemInputView, 0, 0
+            )
 
             try {
                 // Set hint from attributes
                 val hintRef = a.getResourceId(
-                        fr.corenting.edcompanion.R.styleable.SystemInputView_hint,
-                        -1)
-                systemInputEditText.setHint(hintRef)
-                systemInputLayout.hint = context.getString(hintRef)
+                    R.styleable.SystemInputView_hint,
+                    -1
+                )
+                if (hintRef != -1) {
+                    systemInputEditText.setHint(hintRef)
+                    systemInputLayout.hint = context.getString(hintRef)
+                }
+
+                // Cache system name to display on different screens
+                val cacheKeyRef = a.getResourceId(
+                    R.styleable.SystemInputView_cacheKey, -1
+                )
+                val cacheKey = context.getString(cacheKeyRef)
+                setCache(cacheKey)
+
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
@@ -57,7 +74,8 @@ class SystemInputView : RelativeLayout {
 
         // Check if position can be get from commander
         if (PlayerNetworkUtils.setupOk(context) &&
-                PlayerNetworkUtils.getCurrentPlayerNetwork(context).supportLocation()) {
+            PlayerNetworkUtils.getCurrentPlayerNetwork(context).supportLocation()
+        ) {
 
             systemInputLayout.isEndIconVisible = true
             systemInputLayout.setEndIconOnClickListener {
@@ -69,10 +87,35 @@ class SystemInputView : RelativeLayout {
 
         // Set autocomplete view
         systemInputEditText.threshold = 3
-        systemInputEditText.setAdapter(AutoCompleteAdapter(context,
-                AutoCompleteAdapter.TYPE_AUTOCOMPLETE_SYSTEMS))
+        systemInputEditText.setAdapter(
+            AutoCompleteAdapter(
+                context,
+                AutoCompleteAdapter.TYPE_AUTOCOMPLETE_SYSTEMS
+            )
+        )
         systemInputEditText.setOnItemClickListener { adapterView, _, position, _ ->
             systemInputEditText.setText(adapterView.getItemAtPosition(position) as String)
+        }
+    }
+
+    private fun setCache(cacheKey: String) {
+        // Set listener
+        systemInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                SettingsUtils.setString(context, cacheKey, s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        // Fetch and set existing value if any
+        val existingValue = SettingsUtils.getString(context, cacheKey)
+        if (!existingValue.isNullOrEmpty()) {
+            systemInputEditText.setText(existingValue)
         }
     }
 
@@ -100,8 +143,10 @@ class SystemInputView : RelativeLayout {
             systemInputEditText.setText(event.systemName)
         } else {
             val activity = this.findViewById<View>(android.R.id.content).context as Activity
-            NotificationsUtils.displaySnackbar(activity,
-                    context.getString(fr.corenting.edcompanion.R.string.my_location_error))
+            NotificationsUtils.displaySnackbar(
+                activity,
+                context.getString(fr.corenting.edcompanion.R.string.my_location_error)
+            )
 
         }
     }
